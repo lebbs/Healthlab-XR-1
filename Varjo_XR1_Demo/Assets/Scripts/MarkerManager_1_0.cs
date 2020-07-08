bf9e95ef1bbfdfc891b6e835099be61a7172cae4
lebbs <35425109+lebbs@users.noreply.github.com>
2020-07-08 15:54:07 +0300

    123

--- Varjo_XR1_Demo/Assets/Scripts/MarkerManager_1_0.cs
+++ Varjo_XR1_Demo/Assets/Scripts/MarkerManager_1_0.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 using Varjo.XR;
+using System.Linq;
 
 public class MarkerManager_1_0 : MonoBehaviour
 {
@@ -16,6 +18,9 @@ public class MarkerManager_1_0 : MonoBehaviour
 
     public bool markersEnabled = true;
     private bool _markersEnabled;
+
+    public long markerTimeout = 3000;
+    private long _markerTimeout;
 
     private Transform markerTransform;
     // Start is called before the first frame update
@@ -35,7 +40,7 @@ public class MarkerManager_1_0 : MonoBehaviour
         
         if(markersEnabled != _markersEnabled)
         {
-            markersEnabled = VarjoMarkers.EnableVarjomarkers(markersEnabled);
+            markersEnabled = VarjoMarkers.EnableVarjoMarkers(markersEnabled);
             _markersEnabled = markersEnabled;
         }
 
@@ -48,10 +53,69 @@ public class MarkerManager_1_0 : MonoBehaviour
             {
                 foreach( var marker in markers)
                 {
+                    markerIds.Add(marker.id);
+                    if (markerVisualizers.ContainsKey(marker.id))
+                    {
+                        UpdateMarkerVisualizer(marker);
+                    }
+                    else
+                    {
+                        CreateMarkerVisualizer(marker);
+                        VarjoMarkers.SetVarjoMarkerTimeout(marker.id, markerTimeout);
+                    }
+                }
 
+                VarjoMarkers.GetRemovedVarjoMarkerIds(out absentIds);
+
+                foreach (var id in absentIds)
+                {
+                    if (markerVisualizers.ContainsKey(id))
+                    {
+                        Destroy(markerVisualizers[id].gameObject);
+                        markerVisualizers.Remove(id);
+                    }
+                    markerIds.Remove(id);
+                }
+                absentIds.Clear();
+            }
+
+            if (markerIds.Count == 0 && markerVisualizers.Count > 0)
+            {
+                var ids = markerVisualizers.Keys.ToArray();
+                foreach (var id in ids)
+                {
+                    Destroy(markerVisualizers[id].gameObject);
+                    markerVisualizers.Remove(id);
                 }
             }
         }
 
+        void CreateMarkerVisualizer(VarjoMarker marker)
+        {
+
+            GameObject go = Instantiate(markerPrefab);
+            markerTransform = go.transform;
+            go.name = marker.id.ToString();
+            markerTransform.SetParent(xrRig);
+            MarkerVisualizer visualizer = go.GetComponent<MarkerVisualizer>();
+            markerVisualizers.Add(marker.id, visualizer);
+            visualizer.SetMarkerData(marker);
+        }
+
+        void UpdateMarkerVisualizer(VarjoMarker marker)
+        {
+            markerVisualizers[marker.id].SetMarkerData(marker);
+        }
+
+        void SetMarkerTimeOuts()
+        {
+            for (var i = 0; i < markerIds.Count; i++)
+            {
+                VarjoMarkers.SetVarjoMarkerTimeout(markerIds[i], markerTimeout);
+            }
+        }
     }
-}
+        }
+
+    
+
