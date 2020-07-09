5d15437d372c87f7cb52e7c685742db6e47c0592
lebbs <35425109+lebbs@users.noreply.github.com>
2020-07-09 11:30:05 +0300

    muokkaukisa

---
+++ Varjo_XR1_Demo/Assets/Scripts/CubeGame/CubeCheckRed.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class CubeCheckRed : MonoBehaviour
+{
+    public GameObject UiObjectRight;
+    public GameObject UiObjectWrong;
+    public GameObject Trigger;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UiObjectRight.SetActive(false);
+        UiObjectWrong.SetActive(false);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        StartCoroutine(ShowAndHide(UiObjectRight, 1.0f));
+
+
+        if(other.tag == "RedCube")
+        {
+            UiObjectRight.SetActive(true);
+        }
+        else
+        {
+            UiObjectRight.SetActive(false);
+            UiObjectWrong.SetActive(true);
+        }
+    }
+
+
+
+
+    IEnumerator ShowAndHide(GameObject gameObject, float delay)
+    {
+        gameObject.SetActive(true);
+        yield return new WaitForSeconds(delay);
+        gameObject.SetActive(false);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        UiObjectRight.SetActive(false);
+        UiObjectWrong.SetActive(false);
+    }
+
+
+}
