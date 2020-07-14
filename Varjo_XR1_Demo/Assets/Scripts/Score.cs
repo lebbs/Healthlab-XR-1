fe962ddc033599e77755fbab6cd5b5fb4c16ae77
lebbs <35425109+lebbs@users.noreply.github.com>
2020-07-14 15:55:33 +0300

    Pallopeliä jatkettu eteenpäin

---
+++ Varjo_XR1_Demo/Assets/Scripts/Score.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public GameObject scoreText;
+    public int theScore;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Target")
+        {
+            theScore += 1;
+            scoreText.GetComponent<TextMesh>().text = "Score: " + theScore + "/" + "8";
+
+            if (theScore == 8)
+            {
+                theScore = 0;
+            }
+        }
+    }
+}
